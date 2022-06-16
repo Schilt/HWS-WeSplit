38605abcd401dce6cd5e4814bda4a7f7735d5b2f
Andrew A. Schilt <Software.Intelligence@gmail.com>
2022-06-16 14:18:10 -0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Andrew A. Schilt on 6/16/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
